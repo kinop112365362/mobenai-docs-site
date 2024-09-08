7392d5e3543fabedc7c3c76560a01cd9d4576a2d
Shi Hong <shihong@mobenai.com.cn>
2024-09-09 02:23:45 +0800

    ### 重构：更新项目链接和移除repository元数据
    - 移除package.json中的repository、author、bugs和homepage字段。
    - 更新theme.config.tsx中的项目logo和footer文本。
    - 将docsRepositoryBase从GitHub的shuding/nextra-docs-template修改为mobenai/mo-ai-stuidio。
    
    此更改简化了package.json，并对主题配置进行了适度调整，以指向新的项目存储库位置。

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from "nextra-theme-docs"
 const config: DocsThemeConfig = {
   logo: <span>Mobenai</span>,
   project: {
-    link: "https://github.com/kinop112365362/mo-ai-stuidio",
+    link: "https://github.com/mobenai/mo-ai-stuidio",
   },
   // chat: {
   //   link: "https://discord.com",
   // },
   docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: "Mo AI Studio 看见未来...",
+    text: "Mo AI Studio see future...",
   },
   i18n: [
     { locale: "en-US", text: "English" },
